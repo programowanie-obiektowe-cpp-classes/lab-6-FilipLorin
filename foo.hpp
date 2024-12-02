35c8d6ccbdc587767b89e81260d7fe48fb77458c
Mr-Moose-dev <filip.lorin@gmail.com>
2024-12-02 16:55:47 +0100

    solved

--- foo.hpp
+++ foo.hpp
@@ -11,6 +11,7 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > retval(people.size());
 
     // Twoja implementacja tutaj
+    std::transform( people.rbegin(), people.rend(), retval.begin(),  [&](Human& h) { h.birthday(); return h.isMonster()? 'n' : 'y'; } );
 
     return retval;
 }
